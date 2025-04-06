b1f4c2ca9665deaa62075e283f4a0a764040a8f0
BorimirGanchev <borimir.ganchev@gmail.com>
2025-04-06 11:43:31 +0300

    New changes

--- frontendReact/src/components/MapComponent.jsx
+++ frontendReact/src/components/MapComponent.jsx
@@ -164,38 +164,40 @@ const MapComponent = () => {
       </GoogleMap>
 
       <div className="bg-gray-200 flex-grow p-2 m-4 mb-4 rounded-4xl shadow-xl mx-auto max-w-4xl">
-        <h2 className="text-3xl font-bold text-center">Route Information</h2>
-        <div className="flex flex-row gap-50 justify-center items-center">
-          {totalDistance && <p className="text-2xl"><strong>Total Distance:</strong> {totalDistance}</p>}
-          {totalTime && <p className="text-2xl"><strong>Total Time:</strong> {totalTime}</p>}
-        </div>
+        <h2 className="text-3xl font-bold text-center p-4">Route Information</h2>
+        <div className="flex md:flex-row flex-col md:gap-0 gap-10 justify-between items-center p-4">
+          <div className="flex flex-col justify-center items-start text-left">
+            {totalDistance && <p className="text-2xl"><strong>Total Distance:</strong> {totalDistance}</p>}
+            {totalTime && <p className="text-2xl"><strong>Total Time:</strong> {totalTime}</p>}
+          </div>
 
-        <div className="flex justify-center">
-          <a
-            href={generateGoogleMapsLink()}
-            className="group relative inline-flex items-center justify-center overflow-hidden rounded-xl border-2 border-[#003B46] p-4 px-5 py-2.5 font-medium text-[#C4DFE6] shadow-md transition duration-300 ease-out"
-          >
-            <span className="absolute inset-0 flex h-full w-full -translate-x-full items-center justify-center bg-[#003B46] text-white duration-300 group-hover:translate-x-0">
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M14 5l7 7m0 0l-7 7m7-7H3"
-                ></path>
-              </svg>
-            </span>
-            <span className="absolute flex h-full w-full transform items-center justify-center text-[#003B46] transition-all duration-300 group-hover:translate-x-full">
-              Open in Google Maps
-            </span>
-            <span className="invisible relative">Open in Google maps</span>
-          </a>
+          <div className="flex justify-center">
+            <a
+              href={generateGoogleMapsLink()}
+              className="group relative inline-flex items-center justify-center overflow-hidden rounded-xl border-2 border-[#003B46] p-4 px-5 py-2.5 font-medium text-[#C4DFE6] shadow-md transition duration-300 ease-out"
+            >
+              <span className="absolute inset-0 flex h-full w-full -translate-x-full items-center justify-center bg-[#003B46] text-white duration-300 group-hover:translate-x-0">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M14 5l7 7m0 0l-7 7m7-7H3"
+                  ></path>
+                </svg>
+              </span>
+              <span className="absolute flex h-full w-full transform items-center justify-center text-[#003B46] transition-all duration-300 group-hover:translate-x-full">
+                Open in Google Maps
+              </span>
+              <span className="invisible relative">Open in Google maps</span>
+            </a>
+          </div>
         </div>
       </div>
     </LoadScript>
