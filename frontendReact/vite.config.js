0acec8c01071a0bb31af7adeba9b32be340b9c6a
Borimir Ganchev <borimir.ganchev@gmail.com>
2025-12-18 10:26:32 +0200

    Update vite configuration

--- frontendReact/vite.config.js
+++ frontendReact/vite.config.js
@@ -6,19 +6,26 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),],
-    server: {
-      configureServer(server) {
-        server.middlewares.use('/health', (req, res, next) => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/plain');
-          res.end('ok');
-        });
-      },
-      //allowedHosts: 'all',
-      allowedHosts: true,
-      port: 5173,
-      host: "0.0.0.0",
-      //allowedHosts: ['dev.route-optimizer.innovatebulgaria.com', 'localhost']
+    tailwindcss(),
+  ],
+  server: {
+    configureServer(server) {
+      server.middlewares.use('/health', (req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('ok');
+      });
     },
+    allowedHosts: true,
+    port: 5173,
+    host: "0.0.0.0",
+    hmr: {
+      // Use your public domain for the WebSocket connection
+      host: 'dev.route-optimizer.innovatebulgaria.com',
+      // Force the browser to use the secure WebSocket protocol
+      protocol: 'wss',
+      // Use 443 because the browser connects via the HTTPS proxy/load balancer
+      clientPort: 443,
+    },
+  },
 })
